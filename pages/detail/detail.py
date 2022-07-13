fbfc10456c558059cebecb6cfebdc17c5be31c08
NewL <taemin1104@naver.com>
2022-07-13 14:27:13 +0900

    로그인 관련 기능 업데이트 ! main.py / ranking.py / detail.py 수정했음 확인필요함 @토큰 제한 10초 -> 추후 수정 @브라우저 토큰 저장방식 @토큰 미발급시 메인,랭킹,디테일 접근불가 -> 첫 페이지로 강제 이동 (얼럿 없음) 그 외 기능 구현

--- pages/detail/detail.py
+++ pages/detail/detail.py
@@ -5,21 +5,29 @@ blueprint = Blueprint("detail", __name__, url_prefix='/detail')
 
 from pymongo import MongoClient
 import certifi
+import jwt
 
 ca = certifi.where()
 
-client = MongoClient(os.environ.get("CLIENT"), tlsCAFile=ca) #pymongo db 경로
-SECRET_KEY = os.environ.get("PRIVATE_KEY") # Secret key
+client = MongoClient(os.environ.get("CLIENT"), tlsCAFile=ca)  # pymongo db 경로
+SECRET_KEY = os.environ.get("PRIVATE_KEY")  # Secret key
 db = client.dbsparta
 
 
 @blueprint.route("/")
 def detail():
-    return render_template('detail.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+        return render_template('detail.html')
+    except jwt.ExpiredSignatureError:
+        return redirect('/')
+    except jwt.exceptions.DecodeError:
+        return redirect("/")
 
 
 @blueprint.route("/detail", methods=["GET"])
 def detail_get():
     book_list = list(db.bookinfo.find({}, {'_id': False}))
     return jsonify({'book': book_list})
-
