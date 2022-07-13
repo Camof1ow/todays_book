bdfd2afd898ff1723677d11ae35ee3a7ad131018
Camof1ow <astk1203@nate.com>
2022-07-13 14:12:47 +0900

    코멘트 기능 추가

--- pages/detail/detail.py
+++ pages/detail/detail.py
@@ -23,3 +23,20 @@ def detail_get():
     book_list = list(db.bookinfo.find({}, {'_id': False}))
     return jsonify({'book': book_list})
 
+@blueprint.route("/comment", methods=["POST"])
+def bucket_done():
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+    id_receive = request.form['id_give']
+    date_receive = request.form['date_give']
+
+    db.bookcomment.insert_one({'star':star_receive,'comment':comment_receive,'id':id_receive , 'date':date_receive  })
+    return jsonify({'msg': '댓글등록완료!'})
+
+@blueprint.route("/comment", methods=["GET"])
+def comment_get():
+    book_comment = list(db.bookcomment.find({}, {'_id': False}))
+    print(book_comment)
+    return jsonify({'book_comment': book_comment})
+
+
